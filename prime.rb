07cf0ae91b6b506eb639df4df171a3dd2dad87dd
Stephen <stephena9871@gmail.com>
2018-11-08 16:11:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  count = 0
+  if number == -1 or number == 0 or number == 1
+    return false
+  elsif number == 2
+    return true
+  else
+    for x in (2...number)
+      if number % x == 0
+        count += 1
+      end
+    end
+  end
+  if count > 0
+    return false
+  else
+    return true
+  end
+end
