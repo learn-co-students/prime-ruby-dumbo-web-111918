4fc68fdddf661fc7a6c05a72a3c7f5eb90c35516
luijocroussett <ljcroussett@gmail.com>
2018-11-11 00:49:47 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  number = 0-number if number < 0
+  if number == 3 || number == 2
+    return true
+  elsif number < 2
+    return false
+  else
+    prime = true
+    count = number-1
+    while count > 1
+      prime = false if number % count == 0
+      count -= 1
+    end
+  end
+  return prime
+end
