af816ddb5b962b87066d0af4fa77295c350dedcd
github username <github email address>
2018-11-13 22:39:45 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+
+def prime?(number)
+    range = (2..(number - 1)).to_a
+    answer = true
+    if number < 2
+      answer = false
+    else
+      range.each do |num|
+        if number % num == 0
+          answer = false
+          break
+        end
+      end
+    end
+    return answer
+  end
+  
+  prime?(0)
